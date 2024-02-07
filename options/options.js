e0e801502f227d08fe35c61725bb88ec304eb9b2
Crowdbotics <team@crowdbotics.com>
2024-02-07 20:02:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "local-market-maven-46104",
+  url: "https://local-market-maven-46104.botics.co",
+  api: "https://local-market-maven-46104.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
